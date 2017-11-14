28882b8f90bbefc3284fab50ece3c865f48ae0a6
Julia <julia.fried@outlook.com>
2017-11-14 17:25:27 +0100

    assert for utils

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,10 @@
 #' This function calculates the mean of one column
 
 myOwnMean = function(vec){
+  assert(
+    checkNumeric(vec, lower = 1.3, upper = 2.4),
+    checkDataFrame(vec, types = "numeric")
+  )
   m = sum(vec)/length(vec)
   return(m)
 }
